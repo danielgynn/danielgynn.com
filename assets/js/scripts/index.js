ad1216a369e59fbab047207d49681504af0dbb0b
danielgynn <danielgynn@hotmail.co.uk>
2017-06-21 15:05:46 +0100

    Fix back-to-top button

--- assets/js/scripts/index.js
+++ assets/js/scripts/index.js
@@ -7,14 +7,17 @@ $(document).ready(function(){
 	$(window).scroll(function(){
 		if ($(this).scrollTop() > 100) {
 			$('.scroll-top').fadeIn();
+      $('.scroll-top').addClass('flex flex-ac flex-jc');
 		} else {
 			$('.scroll-top').fadeOut();
+      $('.scroll-top').removeClass('flex flex-ac flex-jc');
 		}
 	});
 
 	//Click event to scroll to top
 	$('.scroll-top').click(function(){
 		$('html, body').animate({scrollTop : 0},800);
+    $('.scroll-top').removeClass('flex flex-ac flex-jc');
 		return false;
 	});
 });
