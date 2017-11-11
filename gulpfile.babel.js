f398362f6ecd1f0c96ac3ae63d59e0699b0af30d
Daniel Gynn <danielgynn94@gmail.com>
2017-11-11 13:56:58 +0000

    Redesign 3.0

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -6,20 +6,34 @@ var gulp       = require('gulp'),
     concat     = require('gulp-concat'),
     uglify     = require('gulp-uglify'),
     rename     = require('gulp-rename'),
-    cleanCSS   = require('gulp-clean-css');
+    cleanCSS   = require('gulp-clean-css'),
+    browserify = require('browserify'),
+    source = require('vinyl-source-stream');
+
+const babel = require('gulp-babel');
 
 gulp.task('styles', function() {
   gulp.src('./assets/scss/app.scss')
-  .pipe(sass({
-      style: 'compressed'
-    }))
+  .pipe(sass({style: 'compressed'}))
   .pipe(concat('app.css'))
     .pipe(cleanCSS({compatibility: 'ie8'}))
   .pipe(gulp.dest('./assets/build'));
 });
 
+gulp.task('browserify', function() {
+  return browserify('./assets/build/app.js')
+    .bundle()
+    //Pass desired output filename to vinyl-source-stream
+    .pipe(source('bundle.js'))
+    // Start piping stream to tasks!
+    .pipe(gulp.dest('./assets/build/'));
+});
+
 gulp.task('scripts', function () {
   return gulp.src('./assets/js/scripts/*.js')
+    .pipe(babel({
+      presets: ['env']
+    }))
     .pipe(concat('app.js'))
     .pipe(uglify())
     .pipe(gulp.dest('./assets/build'));
@@ -28,7 +42,7 @@ gulp.task('scripts', function () {
 gulp.task('jshint', function () {
   return gulp.src('./assets/js/*')
     .pipe(jshint())
-    .pipe(jshint.reporter('jshint-stylish'))
+    .pipe(jshint.reporter('jshint-stylish'));
 });
 
 gulp.task('default', ['styles', 'scripts'], function() {
